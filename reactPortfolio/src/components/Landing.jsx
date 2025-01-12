4445552a09d16ebf97840243310c49f004a6718a
this-is-yosuke <yosuke.kibe@gmail.com>
2025-01-12 11:18:36 -0700

    Created the landing page
    The code that came with create Vite was moved to another folder. Will probably delete these later. Created the landing page and put in placeholder text. Created a blank about_me component.

---
+++ reactPortfolio/src/components/Landing.jsx
@@ -0,0 +1,10 @@
+function Landing() {
+    return (
+        <div>
+            <p>Landing page stuff goes here</p>
+            <h1>What the fuck</h1>
+        </div>
+    )
+}
+
+export default Landing;
