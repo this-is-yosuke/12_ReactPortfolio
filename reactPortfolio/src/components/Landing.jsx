a8debc0b1d737b9c7738b85bce787efe67ac2caf
this-is-yosuke <yosuke.kibe@gmail.com>
2025-01-12 15:49:32 -0700

    Updated the AboutMe component
    Added a header. Added a profile image. Added dummy text. Changed styling to align-left. Removed underscore from file name.

--- reactPortfolio/src/components/Landing.jsx
+++ reactPortfolio/src/components/Landing.jsx
@@ -1,8 +1,9 @@
+import About from './AboutMe';
 function Landing() {
     return (
         <div>
             <p>Landing page stuff goes here</p>
-            <h1>What the fuck</h1>
+            <About/>
         </div>
     )
 }
